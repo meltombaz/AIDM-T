439bacf737b50c58b002dd835752d6c3b6061017
meltombaz <melike.tombaz@gmail.com>
2025-09-23 10:06:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with col_lang:
         "Language / Sprache",
         ["🇬🇧 English", "🇩🇪 Deutsch"],   # with flags
         index=0 if st.session_state.lang == "en" else 1,
-        label_visibility="visible"
+        label_visibility="collapsed"
     )
     st.session_state.lang = "en" if "English" in choice else "de"
 LANG = st.session_state.lang
